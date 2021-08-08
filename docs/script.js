20295c576591db66760b5815ea25627e4cf5d811
Nassim Boulechfar <in_boulechfar@esi.dz>
2021-08-08 19:28:25 +0100

    Feat consume api

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,22 @@
-
-
-btn.addEventListener('click',()=>{
-    
-    const btn = document.getElementById('btn');
-    const input = document.querySelector('.input').value;
-    const alert = document.querySelector('.alert');
-
-    let esi = '@esi.dz';
-    if ( input.includes(esi) === false ){
-        alert.innerHTML = 'Your email should contain @esi.dz';
-        input.value = '@esi.dz';
+btn.addEventListener('click', () => {
+    const email_field = document.querySelector(".input");
+    const alert = document.querySelector(".alert");
+    if (email_field.value.split("@")[1] !== "esi.dz"){
+        alert.style.color = "red";
+        alert.innerHTML = "Please provide your @esi.dz email";
+        email_field.value = "@esi.dz";
     }
+    else{
+        fetch(`http://127.0.0.1:5000/check?email=${email_field.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            alert.style.color = "green";
+            alert.innerHTML = data.description;
+        })
+        .catch((error) => {
+            alert.style.color = "red";
+            alert.innerHTML = "An error occurred";
+            log('Request failed', error);
+        });
+    };
 })
-
-
