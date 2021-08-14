8e5321073127dfe153d8de148ad3b0ca09fcdc04
Nassim Boulechfar <in_boulechfar@esi.dz>
2021-08-14 19:51:18 +0100

    Feat improve check result responses

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import Flask, abort
 from flask_cors import CORS
 from config import INV_TEAM, JOIN_ESI_HUB_GP_LINK
 from github_api_ops import get_token, get_user_emails, get_user_info, get_team_id, invite_user
-from api_errors import ApiError, not_enough_permissions, code_not_provided
+from api_errors import ApiError, CheckResult, not_enough_permissions, code_not_provided, \
+esi_email_not_found, esi_email_not_verified
 
 app = Flask(__name__)
 CORS(app, ressources={r'*': {'origins': ['https://hubesi.github.io']}})
@@ -30,28 +31,27 @@ def check_invite():
         raise not_enough_permissions
     user_emails = get_user_emails(token["access_token"])
     esi_email = get_esi_email(user_emails)
+    user_info = get_user_info(token["access_token"])
     if esi_email is None:
-        return {
-            "success": False,
-            "error": "Missing email",
-            "description": "To join ESIHub org you must add your @esi.dz email \
-to your Github account"
-        }
+        esi_email_not_found.gh_user = user_info["login"]
+        raise esi_email_not_found
     if not esi_email["verified"]:
-        return {
-            "success": False,
-            "error": "Email not verified",
-            "description": "Your @esi.dz email is not verified in your Github account, \
-please verify it so you can join"
-        }
+        esi_email_not_verified.gh_user = user_info["login"]
+        raise esi_email_not_verified
     pat = os.environ["ORG_PAT"]
     team_id = get_team_id(pat, INV_TEAM)
-    invite_user(pat, get_user_info(token)['id'], [team_id])
-    return {
-        "success": True,
-        "description": "An invitation to join the org has been sent to you"
-    }
+    invite_user(pat, user_info['id'], [team_id])
+    return CheckResult(
+        200,
+        True,
+        user_info["login"],
+        "An invitation to join the org has been created"
+    ).to_dict(), 200
 
 @app.errorhandler(ApiError)
+def handle_apierror(e):
+    return e.to_dict(), e.status_code
+
+@app.errorhandler(CheckResult)
 def handle_apierror(e):
     return e.to_dict(), e.status_code
